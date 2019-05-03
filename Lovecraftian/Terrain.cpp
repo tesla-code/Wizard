4d8e5ae48b8d356a22c428a4de2e948e20784d55
Victor <vsclause@stud.ntnu.no>
2019-05-03 14:27:56 +0200

    Terrain class can now find min and max value of the grey and creates range, i.e we can now read all the values from the png image!

--- Lovecraftian/Terrain.cpp
+++ Lovecraftian/Terrain.cpp
@@ -11,9 +11,14 @@ void Terrain::loadHeightMap(std::string filePath, ImageType imageType)
 	int image_height = 0;
 	int bpp;
 	int nrChannels = 4; // STBI_rgb_alpha is 4
-	unsigned char* imageData = stbi_load(filePath.c_str(), &image_width, &image_height, &bpp, nrChannels);
+	data = stbi_load(filePath.c_str(), &image_width, &image_height, &bpp, nrChannels);
 
-	if (!imageData)
+	// width and height of terrain
+	this->terrainImageWidth = (unsigned int) image_width;
+	this->terrainImageHeight = (unsigned int)image_height;
+	this->size = (unsigned long)(unsigned int)image_width * (unsigned int)image_height;
+
+	if (!data)
 	{
 		std::cout << "ERROR::TEXTURE_LOADING_FAILED " << std::endl;
 	}
@@ -27,11 +32,41 @@ void Terrain::loadHeightMap(std::string filePath, ImageType imageType)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 	glGenerateMipmap(GL_TEXTURE_2D);
 
 	glActiveTexture(0);				 // activate texture unit 0
 	glBindTexture(GL_TEXTURE_2D, 0); // glGenTextures will never give us a id of 0
+
+	scanHeightMap();
+
+}
+
+
+void Terrain::scanHeightMap()
+{
+	unsigned long i;
+	unsigned char tmp_min, tmp_max;
+
+	tmp_min = 255;
+	tmp_max = 0;
+
+	// Loop over all rgb values inn the image
+	for (int i = 0; i < this->size; i++)
+	{
+		if (this->data[i] < tmp_min)
+		{
+			tmp_min = this->data[i];
+		}
+		if (this->data[i] > tmp_max)
+		{
+			tmp_max = this->data[i];
+		}
+	}
+
+	this->min = tmp_min;		// min 41
+	this->max = tmp_max;		// max 255
+	this->range = max - min;	// 204
 
 }
 
--- Lovecraftian/Terrain.h
+++ Lovecraftian/Terrain.h
@@ -6,10 +6,21 @@
 class Terrain
 {
 private:
-	unsigned int mapID;
+	unsigned int mapID;	// not 100% sure we need this
+
+	unsigned int terrainImageWidth;
+	unsigned int terrainImageHeight;
+
+	unsigned long size; // size of the array I guess
+
+	// z dimensions (range = max - min = relief)
+	unsigned char min, max, range;
+
+	unsigned char* data;
 
 public:
 	void loadHeightMap(std::string filePath, ImageType imageType);
+	void scanHeightMap();
 
 	// functions
 	void bind() const;
